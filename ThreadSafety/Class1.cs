014b1d1a7edd22d09084a49d6d670e860fb82cd6
蘇信宇 <joshuahysu@gmail.com>
2019-01-07 14:20:43 +0800

    +

--- ThreadSafety/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace ThreadSafety
-{
-    public class Class1
-    {
-    }
-}
---
+++ ThreadSafety/ConcurrentLong.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadSafety
+{
+    public class ConcurrentLong
+    {
+        private long _threadSafetyLong = new long();
+
+        public long ThreadSafetyLong
+        {
+            get
+            {
+                return Interlocked.Read(ref _threadSafetyLong);
+            }
+            set
+            {
+                Interlocked.Exchange(ref _threadSafetyLong, value);
+            }
+        }
+
+        public ConcurrentLong()
+        {
+        }
+
+
+        public ConcurrentLong(long temp)
+        {
+            _threadSafetyLong = temp;
+        }
+
+        public static ConcurrentLong operator ++(ConcurrentLong a) {
+            Interlocked.Increment(ref a._threadSafetyLong);
+            return a;            
+        }
+
+        public static ConcurrentLong operator --(ConcurrentLong a)
+        {
+            Interlocked.Decrement(ref a._threadSafetyLong);
+            return a;
+        }
+    }
+}
+
